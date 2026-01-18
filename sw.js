166457080638851ed604339ca9d01da78e9f2a86
timreg003 <timotejreguly@gmail.com>
2026-01-18 23:22:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
 const CACHE_NAME = 'spevnik-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
-];
+const ASSETS = ['./', './index.html', './style.css', './script.js', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'];
 
-// Pri inštalácii stiahni webové súbory do pamäte tabletu
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// Keď si offline, poskytni súbory z pamäte namiesto chyby (dinosaura)
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
